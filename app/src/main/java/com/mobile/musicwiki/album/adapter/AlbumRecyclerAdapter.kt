252e770caebe8b45892cca72efe135937dd8b27a
aashutoshkumar <aashutosh.kr.2308@gmail.com>
2023-04-03 10:20:05 +0530

    bug fixes

--- app/src/main/java/com/mobile/musicwiki/album/adapter/AlbumRecyclerAdapter.kt
+++ app/src/main/java/com/mobile/musicwiki/album/adapter/AlbumRecyclerAdapter.kt
@@ -8,19 +8,17 @@ import com.bumptech.glide.Glide
 import com.mobile.musicwiki.R
 import com.mobile.musicwiki.album.adapter.AlbumRecyclerAdapter.AlbumViewHolder
 import com.mobile.musicwiki.album.model.AlbumQuery
-import com.mobile.musicwiki.databinding.ItemAlbumBinding
 import com.mobile.musicwiki.album.model.AlbumResponse
+import com.mobile.musicwiki.databinding.ItemAlbumBinding
 
 class AlbumRecyclerAdapter(
     private val context: Context,
     private val albumResponse: AlbumResponse?,
     private val callBack: ((albumQuery: AlbumQuery) -> Unit?)? = null
-) :
-    RecyclerView.Adapter<AlbumViewHolder>() {
+) : RecyclerView.Adapter<AlbumViewHolder>() {
 
     override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
+        parent: ViewGroup, viewType: Int
     ): AlbumViewHolder {
         val binding = ItemAlbumBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return AlbumViewHolder(binding)
@@ -36,14 +34,12 @@ class AlbumRecyclerAdapter(
         fun bind(position: Int) {
             albumResponse?.albums?.album?.get(position).apply {
                 Glide.with(context).load(this?.image?.get(2)?.text)
-                    .placeholder(R.drawable.bg_white_rounded_rectangle_black_border)
-                    .centerCrop()
-                    .into(binding.ivCover)
+                    .placeholder(R.drawable.bg_white_rounded_rectangle_black_border).centerCrop()
+                    .error(R.drawable.bg_white_rounded_rectangle_black_border).into(binding.ivCover)
                 binding.tvAlbumName.text = this?.name
                 binding.tvArtistName.text = this?.artist?.name
                 val albumQuery = AlbumQuery(
-                    this?.name,
-                    this?.artist?.name
+                    this?.name, this?.artist?.name
                 )
                 binding.root.setOnClickListener {
                     callBack?.invoke(albumQuery)
